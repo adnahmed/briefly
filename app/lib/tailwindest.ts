ec378c72758f7b765141e7c81179db40322ca615
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2024-06-02 03:48:53 +0500

    add design system colors to tailwind config

---
+++ app/lib/tailwindest.ts
@@ -0,0 +1,31 @@
+import { createTools, Tailwindest } from "tailwindest";
+
+export type TailwindCustom = Tailwindest<{
+  screens: "tablet" | "mobile" | "desktop";
+  color:
+    | "blue-dark"
+    | "blue-dark-100"
+    | "blue-dark-200"
+    | "blue-dark-300"
+    | "blue-dark-400"
+    | "blue-dark-500"
+    | "blue-dark-600"
+    | "blue-dark-700"
+    | "blue-dark-800"
+    | "blue-dark-900"
+    | "blue-dark-950"
+    | "blue"
+    | "gray"
+    | "sky"
+    | "orange"
+    | "yellow"
+    | "green-dark"
+    | "green"
+    | "pink"
+    | "teal"
+    | "red"
+    | "purple"
+    | "green-light";
+}>;
+
+export const tw = createTools<TailwindCustom>();
