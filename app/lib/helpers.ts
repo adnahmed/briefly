9c7265a8e10e55145812086ec1e50931fa257700
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2024-06-02 17:33:09 +0500

    add capitalize helper

---
+++ app/lib/helpers.ts
@@ -0,0 +1,8 @@
+export const capitalize = (s: string) =>
+  s
+    .split("-")
+    .map((st) => `${st[0].toUpperCase()}${st.slice(1, st.length)}`)
+    .join("-")
+    .split(" ")
+    .map((st) => `${st[0].toUpperCase()}${st.slice(1, st.length)}`)
+    .join(" ");
