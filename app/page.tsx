ed2cc515519698991bfa85ebc6a4e3da85ee1af4
Noman hassan <nomanhassan@Nomans-MacBook-Pro.local>
2024-06-04 16:17:38 +0800

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+'use client'
+import { BentoSquare } from "./components/Bento";
 export default function Home() {
-  return <main></main>;
+  return <main>
+    <BentoSquare type="brief" />
+    <BentoSquare type="analytic" />
+    <BentoSquare type="management" />
+    <BentoSquare type="marketplace" />
+  </main>;
 }
