7a5563712faccbdeac29a9d1b6fbd286ee72ac1f
Adnan Ahmed Khan <khanadnanahmed01@gmail.com>
2024-06-08 13:21:13 +0500

    Revert "updated"
    This reverts commit ed2cc515519698991bfa85ebc6a4e3da85ee1af4.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-'use client'
-import { BentoSquare } from "./components/Bento";
 export default function Home() {
-  return <main>
-    <BentoSquare type="brief" />
-    <BentoSquare type="analytic" />
-    <BentoSquare type="management" />
-    <BentoSquare type="marketplace" />
-  </main>;
+  return <main></main>;
 }
